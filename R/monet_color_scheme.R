46e37b330a87524e027383d93c740a6cc66228f0
Yanhua Zheng <36413792+rainoffallingstar@users.noreply.github.com>
2024-04-18 19:19:45 +0800

    update

--- R/monet_color_scheme.R
+++ R/monet_color_scheme.R
@@ -39,10 +39,20 @@ monet_color_scheme <- function(num_colors = 5,
       workid <- sample(1:1250,1)
       random_colors <- monet_color_df[workid,] %>% unlist()
       colors <- random_colors[-1]
+      colors <- colors[1:num_colors]
       des <- monet %>% 
         dplyr::filter(id %in% random_colors)
       message(glue::glue("color from the work {des$title[1]} is used"))
     }
   }
+  library(ggplot2)
+  p <- data.frame(color_vector=colors ,
+           num = rep(1,num_colors)) %>% 
+    ggplot(aes(x = num, y = num)) + 
+    geom_bar(stat = "identity", width = 0.7,fill = colors) + 
+    theme_minimal() + 
+    theme(axis.text.x = element_blank(), axis.title.x = element_blank(),
+        axis.ticks.x = element_blank())
+  print(p)
   return(colors)
 }
